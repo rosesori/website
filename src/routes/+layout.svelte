efcad23dc51537874659130dc0d0fd7e986fb51f
Rose Soriano <rosesoriano.dtx@gmail.com>
2024-05-29 00:28:31 -0700

    Created About page

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -17,9 +17,9 @@
       <button class="btn btn-square btn-ghost">
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-5 h-5 stroke-current"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
       </button>
-      <ul tabindex="0" class="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+      <ul class="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
         <li><a href="/">Home</a></li>
-        <li><a href="/">About</a></li>
+        <li><a href="/about">About</a></li>
       </ul>
     </div>
   </div>
@@ -34,4 +34,4 @@
 </div>
 
 
-<slot />
+<slot></slot>
---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,9 @@
+<div class="hero min-h-screen bg-base-200">
+  <div class="hero-content flex-col lg:flex-row-reverse">
+    <div>
+      <h1 class="text-5xl font-bold">About the Creator</h1>
+      <p class="py-6">Hi, my name is Rose Soriano. Thank you for trying out my website! I am a software engineer based out of southern California, but I am originally from Texas where I eventually got my degree in Computer Engineering.</p>
+      <p class="py-6">I started cubing when I was in elementary school after a classmate had solved a Rubik's cube full of jelly (for lube) in front of the class. Interested in seeing how hard solving one really was, I learned CFOP from YouTube videos. I ultimately fell off of cubing amongst other hobbies as I got older, but I have gotten back into cubing again after graduating from college. I was shocked to find out stickerless cubes are now competition legal! During this time of learning more advanced CFOP techniques, I realized that I struggle to learn with the existing resources out there. From a programmers perspective, I aim to create learning tools that I think provide a more intuitive and efficient user experience.</p>
+    </div>
+  </div>
+</div>
